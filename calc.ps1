dc2cfddc7beec8a3b949517ab45517756fe95185
maliikmalik <abdel.malik.attaf@outlook.com>
2018-05-14 15:43:20 +0200

    new file

---
+++ calc.ps1
@@ -0,0 +1,38 @@
+﻿write-host "bienvenu dans la calculette"
+
+[int]$nb1 = read-host "nombre1"
+[int]$nb2 = read-host "nombre2"
+$opérateur = Read-Host "opérateur"
+
+
+    switch($opérateur)
+    { 
+        
+        "-"
+        {
+            $resultat = $nb1 - $nb2
+        }
+
+        
+        "+"
+        {
+            $resultat = $nb1 + $nb2
+        }
+        
+       
+        "*"
+        {
+            $resultat = $nb1 * $nb2
+        }
+
+       
+        "/"
+        {
+            $resultat = $nb1 / $nb2
+        }
+    }
+
+write-host $resultat
+
+
+
